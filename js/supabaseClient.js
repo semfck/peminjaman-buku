2969ccb8e50e2c025d66b507cb6bef5f80ddd206
semfck semi <newgithub58@gmail.com>
2025-05-15 04:38:25 +0000

    main

---
+++ js/supabaseClient.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = 'https://xqnlchcbxekwulncjvfy.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxbmxjaGNieGVrd3VsbmNqdmZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyNzcxNzksImV4cCI6MjA2Mjg1MzE3OX0.j8nyrPIp64bJL_WziUE8ceSvwrSU0C8VHTd4-qGl8D4'
+export const supabase = createClient(supabaseUrl, supabaseKey)
